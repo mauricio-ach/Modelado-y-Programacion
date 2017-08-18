c2c34591ee11f6f034e7e94743a62a8098674e97
Mauricio Araujo <mauricio.ach@ciencias.unam.mx>
2017-08-17 21:29:31 -0500

    Primer Hola Mundo: Java

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+/*
+* Clase que imprime un hola mundo
+* en salida estandar
+* @author Mauricio
+*/
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+	
+}
