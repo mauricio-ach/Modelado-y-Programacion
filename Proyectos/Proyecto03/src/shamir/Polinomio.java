f372d7925d5657a287fb70a216d6904d8b1b5341
Mauricio <mauricio.ach@ciencias.unam.mx>
2018-01-12 18:39:55 -0600

    Estructura del proyecto

---
+++ Proyectos/Proyecto03/src/shamir/Polinomio.java
@@ -0,0 +1,32 @@
+package shamir;
+
+/**
+ * Polinomio.java 
+ * Version 1.0
+ * Copyright (C)  2017 by Mauricio Araujo Chavez
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.math.BigInteger;
+/**
+ * @author Mauricio Araujo
+ * @version 1.0 
+ * @since Jan 2 2018
+ *
+ * Clase para modelar un polinomio de grado n
+ *
+ */
+public class Polinomio{}
